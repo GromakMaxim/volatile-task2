583d7f780eb69a5085f85044ec73ec0ef699578d
Gromak Maxim <maximgromak@gmail.com>
2020-12-19 00:16:17 +1000

    fixed

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,18 +1,33 @@
 package org.example;
 
-import org.example.shops.Shop1;
-import org.example.shops.Shop2;
-import org.example.shops.Shop3;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App {
+    private int THREADS_NUMBER = 3;
+
     public static void main(String[] args) throws InterruptedException {
+        App myApp = new App();
+        LongAdder balance = new LongAdder();
+        List<Shop> shopList = Stream.of(new Shop("Магазин1"),
+                new Shop("Магазин2"),
+                new Shop("Магазин3"))
+                .collect(Collectors.toList());
 
-        Shop1 sh1 = new Shop1();
-        Shop2 sh2 = new Shop2();
-        Shop3 sh3 = new Shop3();
+        ExecutorService executorService = Executors.newFixedThreadPool(myApp.THREADS_NUMBER);//создаём исполнитель
 
-        ThreadCalculator calculator = new ThreadCalculator(sh1, sh2, sh3);
+        shopList.stream()
+                .map(Shop::getBalance)
+                .forEach(shopBalance -> shopBalance
+                        .forEach(item -> executorService.submit(() -> balance.add(item))));
 
-        System.out.println("Сумма по всем магазинам: " + calculator.getMoney().get());
+        executorService.shutdown();
+        executorService.awaitTermination(10, TimeUnit.SECONDS);
+        System.out.println("\nРезультат: " + balance.sum());
     }
 }
--- src/main/java/org/example/Shop.java
+++ src/main/java/org/example/Shop.java
@@ -1,20 +1,23 @@
-package org.example.shops;
+package org.example;
 
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
-public class Shop1 {
+public class Shop {
     private final List<Integer> balance;
     private final int VALUES_LIMIT = 1_000_000;
     private final int LOW_LIMIT = 0;
     private final int UPPER_LIMIT = 100;
 
-    public Shop1() {
+    private String title;
+
+    public Shop(String title) {
         this.balance = new Random().ints(LOW_LIMIT, UPPER_LIMIT)
                 .limit(VALUES_LIMIT)
                 .boxed()
                 .collect(Collectors.toList());
+        this.title = title;
     }
 
     public List<Integer> getBalance() {
